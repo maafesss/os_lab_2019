35406787cbc234a003040157d34e004820ac26ee
maafesss <maafesss@bk.ru>
2026-06-01 17:49:05 +0000

    Lab7: TCP/UDP client-servers

--- lab7/src/udp_client.c
+++ lab7/src/udp_client.c
@@ -4,46 +4,34 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define BUFFER_SIZE 1024
-
 int main(int argc, char *argv[]) {
-    char *server_ip = "127.0.0.1";
+    char *ip = "127.0.0.1";
     int port = 9090;
     
-    if (argc > 1) server_ip = argv[1];
+    if (argc > 1) ip = argv[1];
     if (argc > 2) port = atoi(argv[2]);
     
     int sock;
     struct sockaddr_in server_addr;
     socklen_t server_len = sizeof(server_addr);
-    char buffer[BUFFER_SIZE];
+    char buffer[1024];
     
-    // Создание UDP сокета
     sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) {
-        perror("socket failed");
-        exit(1);
-    }
+    if (sock < 0) { perror("socket failed"); exit(1); }
     
-    // Настройка адреса сервера
-    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
     
-    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
-        perror("inet_pton failed");
-        exit(1);
+    if (inet_pton(AF_INET, ip, &server_addr.sin_addr) <= 0) {
+        perror("inet_pton failed"); exit(1);
     }
     
-    printf("UDP client sending to %s:%d\n", server_ip, port);
+    printf("UDP client sending to %s:%d\n", ip, port);
     
-    // Отправляем сообщение
-    char message[] = "Hello from UDP client!";
-    sendto(sock, message, strlen(message), 0,
+    sendto(sock, "Hello from UDP client!", 22, 0,
            (struct sockaddr*)&server_addr, server_len);
-    printf("Sent: %s\n", message);
+    printf("Message sent\n");
     
-    // Получаем ответ
     int bytes = recvfrom(sock, buffer, sizeof(buffer) - 1, 0,
                          (struct sockaddr*)&server_addr, &server_len);
     if (bytes > 0) {
